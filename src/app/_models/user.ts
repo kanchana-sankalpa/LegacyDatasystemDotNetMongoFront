54eeead07cf9bfe6a9c75fd03ae922ad53633272
Catchme <mdksankalpa@gmail.com>
2020-11-09 12:30:53 +0930

    Initial commit

---
+++ src/app/_models/user.ts
@@ -0,0 +1,13 @@
+﻿import { Role } from "./role";
+
+export class User {
+    id: number;
+    userId: number;
+    username: string;
+    password: string;
+    firstName: string;
+    fullName: string;
+    lastName: string;
+    role: Role;
+    token?: string;
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+﻿import { Component } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '@app/_models';
+import { UserService, AuthenticationService } from '@app/_services';
+
+@Component({ templateUrl: 'home.component.html' })
+export class HomeComponent {
+    loading = false;
+    user: User;
+    userFromApi: User;
+
+    constructor(
+        private userService: UserService,
+        private authenticationService: AuthenticationService
+    ) {
+        this.user = this.authenticationService.userValue;
+    }
+
+    ngOnInit() {
+        this.loading = true;
+        console.log(this.user);
+        this.userService.getById(this.user.userId).pipe(first()).subscribe(user => {
+            this.loading = false;
+            this.userFromApi = user;
+        });
+    }
+}
